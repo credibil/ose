d811c5fe1458e9019baa9b39d087f4cb925c8b89
Andrew Goldie <goldie@worksend.com>
2025-05-06 16:59:01 +1200

    move algorithm-specific verification from jose

--- src/signing.rs
+++ src/signing.rs
@@ -2,6 +2,8 @@
 
 use std::fmt::Display;
 
+use anyhow::anyhow;
+use ecdsa::signature::Verifier as _;
 use serde::{Deserialize, Serialize};
 
 /// The signing algorithm used by the signer.
@@ -20,6 +22,33 @@ pub enum Algorithm {
 impl Display for Algorithm {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{self:?}")
+    }
+}
+
+/// Verifications
+impl Algorithm {
+    /// Verify the signature of a signed message.
+    ///
+    /// # Errors
+    /// Will return an error if the signature is invalid or the verifying key is
+    /// not correct for the type of algorithm.
+    pub fn verify(&self, msg: &[u8], sig: &[u8], pub_key: &[u8]) -> anyhow::Result<()> {
+        match self {
+            Self::ES256K => {
+                let verifying_key =
+                    ecdsa::VerifyingKey::<k256::Secp256k1>::from_sec1_bytes(pub_key)?;
+                let signature = ecdsa::Signature::<k256::Secp256k1>::from_slice(sig)?;
+                let normalized = signature.normalize_s().unwrap_or(signature);
+                Ok(verifying_key.verify(msg, &normalized)?)
+            }
+            Self::EdDSA => {
+                let pk_bytes =
+                    pub_key.try_into().map_err(|_| anyhow!("invalid public key length"))?;
+                let verifying_key = ed25519_dalek::VerifyingKey::from_bytes(&pk_bytes)?;
+                let signature = ed25519_dalek::Signature::from_slice(sig)?;
+                Ok(verifying_key.verify(msg, &signature)?)
+            }
+        }
     }
 }
 
